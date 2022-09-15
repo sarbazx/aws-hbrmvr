9704d200a8149c8061a9544fe5659365a8a5283b
Taskhyn Maksim <tasqynm@gmail.com>
2022-09-15 19:24:50 +0300

    scroll top

--- src/haber/NewsView.tsx
+++ src/haber/NewsView.tsx
@@ -1,11 +1,19 @@
-import { Card, Group, Text } from '@mantine/core';
-import { useMatch } from '@tanstack/react-location';
+import { Anchor, Card, Group, Text } from '@mantine/core';
+import { useMatch, useRouter } from '@tanstack/react-location';
+import { useEffect } from 'react';
 import RecommendedNews from './RecommendedNews';
 
 const NewsView = () => {
   const {
     data: { news },
   } = useMatch();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [router.state.location.pathname]);
+
   return (
     <>
       <Card shadow='sm' p='lg' radius='md' m='lg' withBorder>
@@ -14,6 +22,9 @@ const NewsView = () => {
             {news[0].title}
           </Text>
           <div>{news[0].content}</div>
+          <Anchor href={news[0].url} target='_blank'>
+            Read More
+          </Anchor>
         </Group>
       </Card>
       <Text weight={700} size='lg' p='lg'>
